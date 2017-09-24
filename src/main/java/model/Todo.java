008ebc1be7ea2dd7be879eb825decb9752beea13
adamFoldvari <foldadam93@gmail.com>
2017-09-24 11:12:04 +0200

    Fix missing getters and setters

--- src/main/java/model/Todo.java
+++ src/main/java/model/Todo.java
@@ -11,6 +11,30 @@ public class Todo {
     String id;
     Status status;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     public void toggleStatus() {
         this.status = isComplete() ? Status.ACTIVE : Status.COMPLETE;
     }
